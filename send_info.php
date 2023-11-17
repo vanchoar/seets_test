eebe5973ba345e0e13720e6be196c7af95648b5f
vanchoar <vanco.ar@gmail.com>
2023-11-17 11:42:59 +0200

    rollback working version

--- send_info.php
+++ send_info.php
@@ -89,36 +89,27 @@ $years = $new_years;
   // Christmas weekday encryption
 
 class Encryptions {
-  private $encryption_method = "";
-  private readonly $key;
-
-  private function __construct(string $encryption_method)
-  {
-    $this->encryption_method = $encryption_method;
-    $this->key = "your_amazing_key_here";
-  }
-
-  public static function Create128AESEncryption(): Encryptions{
-    return new Encryptions("AES-128-CBC");
-  }
+  public $encryption_method = "AES-128-CBC";
+  public $key = "your_amazing_key_here";
 
   public function encrypt($data) {
-      $ivlen = openssl_cipher_iv_length($this->encryption_method);
+
+      $ivlen = openssl_cipher_iv_length($cipher = $this->encryption_method);
       $iv = openssl_random_pseudo_bytes($ivlen);
-      $ciphertext_raw = openssl_encrypt($data, $this->encryption_method, $this->key, $options = OPENSSL_RAW_DATA, $iv);
+      $ciphertext_raw = openssl_encrypt($data, $cipher, $this->key, $options = OPENSSL_RAW_DATA, $iv);
       $hmac = hash_hmac('sha256', $ciphertext_raw, $this->key, $as_binary = true);
       $ciphertext = base64_encode($iv . $hmac . $ciphertext_raw);
       return $ciphertext;
   }
 
   public function decrypt($data) {
-      $ivlen = openssl_cipher_iv_length($this->encryption_method);
+      $ivlen = openssl_cipher_iv_length($cipher = $this->encryption_method);
 
       $c = base64_decode($data);
       $iv = substr($c, 0, $ivlen);
       $hmac = substr($c, $ivlen, $sha2len = 32);
       $ciphertext_raw = substr($c, $ivlen + $sha2len);
-      $original_plaintext = openssl_decrypt($ciphertext_raw, $this->encryption_method, $this->key, $options = OPENSSL_RAW_DATA, $iv);
+      $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $this->key, $options = OPENSSL_RAW_DATA, $iv);
       $calcmac = hash_hmac('sha256', $ciphertext_raw, $this->key, $as_binary = true);
       if (hash_equals($hmac, $calcmac))
       {
@@ -127,7 +118,7 @@ class Encryptions {
   }
 }
 
-$encryptions = Encryptions::Create128AESEncryption();
+$encryptions = new Encryptions();
 
 $full_info = [];
 // get christmas day of each year
