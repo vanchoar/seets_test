f3a016646cb5c7b4c453e881336d895d279191b3
vanchoar <vanco.ar@gmail.com>
2023-11-20 12:07:44 +0200

    add new functions

--- send_info.php
+++ send_info.php
@@ -15,44 +15,19 @@ include('database_operations.php');
 
 
 
-$year = $_POST['year'];
-
 $time_calculations = new Time_calculations();
 
 $encryptions = new Encryptions();
 
 $db_operations = new Database_operations();
 
+$year = $_POST['year'];
+
 // Calculate 30 previous Prime number years
-$years = $time_calculations->calculate_30_primenumber_years($year);
-
-// Select all years already present in database (if there are any)
-$database_entries = $db_operations->select_from_table('year', '');  
-$res = $conn->query($database_entries);
-
-$existing_years = [];
-// format already existing years in database in a list
-while($y = mysqli_fetch_assoc($res)){
-    array_push($existing_years, $y['year']);
-};
-
-// match new years with existing years
-$unique_years = array_unique( array_merge($years, $existing_years) );
-$new_years = array_diff( $unique_years, $existing_years);
-// return only new years, not previously present
-$years = $new_years;
-
+// Get only new years, not previously present
 // calculate + encrypt day christmas day of each year, make list with (years, encrypted days)
-$years_and_encrypted_days = $time_calculations->get_years_and_encrypted_christmas_days($years);
-
 // insert full entries into database
-foreach($years_and_encrypted_days as $year_encr_day){
-    $data_separated = explode(' ', $year_encr_day);
-    $year = mysqli_real_escape_string($conn, $data_separated[0]);
-    $day = mysqli_real_escape_string($conn, $data_separated[1]);
-  // send year + encripted day to database
-    $db_operations->insert_into_table($year, $day);
-}
+$time_calculations->only_add_missing_years($year);
 
 // Select full info (year, day) from database
 $database_entries = $db_operations->select_from_table('year', 'day');
--- time_calculations.php
+++ time_calculations.php
@@ -2,7 +2,7 @@
 
 class Time_calculations { 
     // function calculate until 30 years
-        public function calculate_30_primenumber_years($year){
+        private function calculate_30_primenumber_years($year){
             $years = [];
     
             $math = new Math();
@@ -24,7 +24,7 @@ class Time_calculations {
         }
     
       // get christmas day of each year from $years list
-        public function get_years_and_encrypted_christmas_days($years){    
+        private function get_years_and_encrypted_christmas_days($years){    
             $years_and_encrypted_days = [];
             $encryptions = new Encryptions();
     
@@ -39,6 +39,43 @@ class Time_calculations {
                 }
     
             return $years_and_encrypted_days;
+        }
+
+        public function only_add_missing_years($year){
+
+            // Calculate 30 previous Prime number years
+            $years = $this->calculate_30_primenumber_years($year);
+
+            // Select all years already present in database (if there are any)
+            require('db.php');
+            $db_operations = new Database_operations();
+            $database_entries = $db_operations->select_from_table('year', '');  
+            $res = $conn->query($database_entries);
+
+            $existing_years = [];
+            // format already existing years in database in a list
+            while($y = mysqli_fetch_assoc($res)){
+                array_push($existing_years, $y['year']);
+            };
+
+            // match new years with existing years
+            $unique_years = array_unique( array_merge($years, $existing_years) );
+            $new_years = array_diff( $unique_years, $existing_years);
+            // return only new years, not previously present
+            $years = $new_years;
+
+            // calculate + encrypt day christmas day of each year, make list with (years, encrypted days)
+            $years_and_encrypted_days = $this->get_years_and_encrypted_christmas_days($years);
+
+            // insert full entries into database
+            foreach($years_and_encrypted_days as $year_encr_day){
+                $year_day_separated = explode(' ', $year_encr_day);
+                $year = mysqli_real_escape_string($conn, $year_day_separated[0]);
+                $day = mysqli_real_escape_string($conn, $year_day_separated[1]);
+              // send year + encripted day to database
+                $db_operations->insert_into_table($year, $day);
+            }
+
         }
 }
 
